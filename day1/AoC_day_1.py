cd7d6dc66160a303133b3f40ea2ce5965c2448d3
Jeffrey Zocher <jeff.zocher@gmail.com>
2022-12-01 18:54:42 -0600

    Challenge completed

--- day1/AoC_day_1.py
+++ day1/AoC_day_1.py
@@ -1,30 +1,33 @@
-infile = "AoC_day_1_input.txt"
-answer = ""
+infile = "AoC_day_1_input.txt" # File contining the challenge input
 
 
 def parse_elves():
+    # Initialize variables
     elves = []
-    elf_num = 0
-    tmp_arr = []
+    tmp = []
     escaped = "\n"
+    totals = {}
+    elf_num = 0
+    # Open the text file contining the challenge inputs
     with open(f'{infile}', 'r') as fin:
-        for line in fin:
-            # print(f"Parsing line {line}")
-            # separate the elves
-            if line == escaped:
-                ("new line found")
-                elves.append(tmp_arr)
-                # tmp_arr.clear()
-                elf_num += 1
+        for line in fin: # Parse the file line by line
+            if line == escaped: # If the line is blank (\n)
+                elves.append(tmp) # Add the list of the previous elf's calories to the full list
+                tmp = [] # Reset the temp list by creating a new one
+                # If a new list is not initialized, then the previous list(s) added to the full list will be modified by new values as well
             else:
-                # print("Added to array")
-                line = line.replace(escaped, '')
-                int(line)
-                tmp_arr.append(line)
-        print("Completed parsing")
-    fin.close()
-    for elf in elves:
-        print(elves[elf])
+                line = line.replace(escaped, '') # remove new line characters for int convertion
+                tmp.append(int(line)) # Add the item, convert to int, to the temp list
+    fin.close() # Close the file
+    for elf in elves: # Parse through the full list of elves
+        total = 0
+        for i in range (0, len(elf)): # Loop through the items for each elf and add them up
+            total += elf[i]
+        totals[elf_num] = total # Add the total calories of each elf to a dict with their corresponding number
+        elf_num += 1
+    most_cal = max(totals.items(), key=lambda x: x[1]) # Finds the highest calorie value in the dict
+    # [print(key,':',value) for key, value in totals.items()] # Prints all dict keys and values
+    print(f'\n{most_cal}') # Prints out the elf with the most calories and the amount of calories
 
 
 if __name__ == "__main__":
